222ca5e0a554a1a2321a9c57e314e13c98c924b5
anoca1011 <anoca1011@gmail.com>
2026-03-26 09:04:56 -0300

    ex11'

--- Ex11.java
+++ Ex11.java
@@ -5,13 +5,11 @@ public class Ex11{
 
         int id;
 
-        System.out.println("menores de 12 = 10,00");
-        System.out.println("entre 12 e 60 = 20,00");
-        System.out.println("acima de 60 = 10,0");
-
         System.out.println("informe sua idade e consulte o valor da entrada ao cinema: ");
         id = scanner.nextInt();
 
         String val = (id < 12)? "10,00" : (id > 12 && id < 60 )? "20,00" : "10,00";
+
+        System.out.println("voce ira pagar $" +val);
     }
 }
