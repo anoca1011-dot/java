c64c9a121c9d42e9ab72a07ff2305da3d1ba9993
anoca1011 <anoca1011@gmsil.com>
2026-03-31 14:52:26 -0300

    ex14

---
+++ Ex14.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ex14{
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+
+        int n1;
+        int n2;
+        int n3;
+
+        System.out.println("informe um numero: ");
+        n1 = scanner.nextInt();
+        System.out.println("informe outro numero: ");
+        n2 = scanner.nextInt();
+        System.out.println("informe mais um numero: ");
+        n3 = scanner.nextInt(); 
+    }
+}
