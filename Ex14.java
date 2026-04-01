64be5f5bf21629418bfd5102d6517b1129fccf2e
anoca1011 <anoca1011@gmail.com>
2026-04-01 08:41:11 -0300

    ex14

--- Ex14.java
+++ Ex14.java
@@ -12,6 +12,22 @@ public class Ex14{
         System.out.println("informe outro numero: ");
         n2 = scanner.nextInt();
         System.out.println("informe mais um numero: ");
-        n3 = scanner.nextInt(); 
+        n3 = scanner.nextInt();
+
+        if(n1>n2 && n2>n3){
+            System.out.println("o " +n1+ " é maior");
+        }else if(n1==n2 && n2>n3){
+            System.out.println("o " +n1+ "," +n2+ " estao empatados");
+        }else if(n1==n3 && n3>n2){
+            System.out.println("o " +n1+ "," +n3+ " estao empatados");
+        }else if(n2>n1 && n2>n3){
+            System.out.println("o " +n2+ " é maior");
+        }else if(n2==n3 && n3>n1){
+            System.out.println("o " +n2+ "," +n3+ " estao empatados");
+        }else if(n3>n1 && n3>n2){
+            System.out.println("o " +n3+ " é maior");
+        }else{
+            System.out.println("todos estao empatados");
+        }
     }
 }
