7a50bcd2fafd547e508439a700faefa0354861aa
anoca1011 <anoca1011@gmail.com>
2026-04-06 10:49:02 -0300

    ex17inc

---
+++ Ex17.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ex17{
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+
+        double val;
+        double vad;
+        int de;
+
+        //mais que 500= 20%
+        //mais que 200 - 500 = 10%
+        //menos que 200 = 0%
+        //informe o valor final com desconto
+        System.out.println("informe o valor de compra: ");
+        val = scanner.nextDouble();
+
+    }
+}
