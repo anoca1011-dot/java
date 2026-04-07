95a4c8466960237e104e3e58fc083ebcea3b2916
anoca1011 <anoca1011@gmail.com>
2026-04-07 14:50:08 -0300

    ex17

--- Ex17.java
+++ Ex17.java
@@ -4,15 +4,22 @@ public class Ex17{
         Scanner scanner = new Scanner(System.in);
 
         double val;
-        double vad;
-        int de;
+        double vad = 0;
+        double valf = 0;
 
-        //mais que 500= 20%
-        //mais que 200 - 500 = 10%
-        //menos que 200 = 0%
-        //informe o valor final com desconto
-        System.out.println("informe o valor de compra: ");
+        System.out.println("informe o valor para darmos desconto; ");
         val = scanner.nextDouble();
 
+        if (val > 500){
+            vad = val * 20 / 100;
+            valf = val - vad;
+            System.out.println("o desconto é de 20%, valor final $" +valf);
+        }else if (val > 200 && val <= 500){
+            vad = val * 10 / 100;
+            valf = val - vad;
+            System.out.println("o desconto é de 10%, valor final $" +valf);
+        }else{
+            System.out.println("o desconto é inesistente, valor final $" +val);
+        }
     }
 }
