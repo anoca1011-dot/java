fc4e939a8989a898b92316f144bc52c0f7d30146
anoca1011 <anoca1011@gmail.com>
2026-04-07 14:57:02 -0300

    inc

---
+++ Ex18.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Ex18{
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+
+        int n;
+
+        System.out.println("");
+    }
+}
