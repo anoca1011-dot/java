998375752f16fdac6b256d0c27618c851e7f1cd3
anoca1011 <anoca1011@gmail.com>
2026-04-08 08:49:21 -0300

    ex18

--- Ex18.java
+++ Ex18.java
@@ -3,8 +3,46 @@ public class Ex18{
     public static void main(String[]args){
         Scanner scanner = new Scanner(System.in);
 
-        int n;
+        int nu;
 
-        System.out.println("");
+        System.out.println("informe um numero de 1-10 para descobrir o equivalente em algarismo romano: ");
+        nu = scanner.nextInt();
+
+        if (nu <1 || nu >10){
+            System.out.println("informe um numero valido(1-10)");
+        }else{
+            switch (nu){
+               case 1:
+                System.out.println("I"); 
+                break;
+                case 2:
+                    System.out.println("II");
+                    break;
+                    case 3:
+                        System.out.println("III");
+                        break;
+                        case 4:
+                            System.out.println("IV");
+                            break;
+                            case 5:
+                                System.out.println("V");
+                                break;
+                                case 6:
+                                    System.out.println("VI");
+                                    break;
+                                    case 7:
+                                        System.out.println("VII");
+                                        break;
+                                        case 8:
+                                            System.out.println("VIII");
+                                            break;
+                                            case 9:
+                                                System.out.println("IX");
+                                                break;
+                                                case 10:
+                                                    System.out.println("X");
+                                                    break;
+            }
+        }
     }
 }
---
+++ Ex19.java
@@ -0,0 +1,14 @@
+//ler a velocidade e o limite da via classificando:
+//dentro do limite -- sem multa
+//20 % acima do limite -- multa leve
+//entre 20% e 50% acima do limite -- multa grave
+// acima de 50% do limite -- multa gravissima + suspensão
+
+import java.util.Scanner;
+public class Ex19{
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+
+        double 
+    }
+}
