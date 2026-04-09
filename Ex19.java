7ed113913261d071db49f326fd1e0cadd0219d93
anoca1011 <anoca1011@gmail.com>
2026-04-09 09:16:22 -0300

    ex19

--- Ex19.java
+++ Ex19.java
@@ -9,6 +9,28 @@ public class Ex19{
     public static void main(String[]args){
         Scanner scanner = new Scanner(System.in);
 
-        double 
+        double liv;
+        double veat;
+        
+        System.out.println("informe a velocidade atingida: ");
+        veat = scanner.nextDouble();
+        System.out.println("informe a velocidade limite da via: ");
+        liv = scanner.nextDouble();
+
+        double mul = veat * liv / 100;
+        
+        if(mul <= 0){
+            System.out.println("vc ficou " +mul+ "% acima do limite");
+            System.out.println("...sem multa...");
+        }else if(mul<=20){
+            System.out.println("vc ficou " +mul+ "% acima do limite");
+            System.out.println("...multa leve...");
+        }else if (mul>20 || mul<=50){
+            System.out.println("vc ficou " +mul+ "% acima do limite");
+            System.out.println("...multa grave...");
+        }else{
+            System.out.println("vc ficou " +mul+ "% acima do limite");
+            System.out.println("...multa gravissima + suspensão...");
+        }
     }
 }
